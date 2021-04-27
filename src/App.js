3047a426bfcad0ed52405de14efe8e4f422c252d
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-04-27 19:41:19 -0400

    add router, link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Particles from 'react-particles-js'
 import Navbar from './components/Navbar';
 import Header from './components/Header'
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
@@ -34,63 +34,16 @@ function App() {
           }
         }}
       />
-      {/* <Particles className="particals-canvas"
-        params={{
-          "particles": {
-              "number": {
-                  "value": 100,
-                  "density": {
-                      "enable": false
-                  }
-              },
-              "color": {
-                "value": "rgb(180, 127, 187)"
-              },
-              "size": {
-                  "value": 5,
-                  "random": true,
-                  "anim": {
-                      "speed": 4,
-                      "size_min": 0.5
-                  }
-              },
-              "line_linked": {
-                  "enable": false
-              },
-              "move": {
-                  "random": true,
-                  "speed": 1,
-                  "direction": "top",
-                  "out_mode": "out"
-              }
-          },
-          "interactivity": {
-              "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "bubble"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "repulse"
-                }
-              },
-              "modes": {
-                "bubble": {
-                    "distance": 250,
-                    "duration": 2,
-                    "size": 0,
-                    "opacity": 0,
-                },
-                "repulse": {
-                    "distance": 400,
-                    "duration": 4
-                }
-              }
-          }
-      }} /> */}
-      <Navbar />
-      <Header />
+      <Router>
+        <Navbar />
+        <Switch> {/* render the first Route component that matches url. If not found, than it renders NotFound component */}
+            <Route exact path="/" component={Header} />
+            {/* <Route path="/about" component={About} />
+            <Route path="/projects" render={ () => <Projects projectData={this.state.projectData}/>}/> 
+            <Route exact path="/contact" component={Contact} />        
+            <Route component={NotFound} /> */}
+          </Switch>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,18 @@
-import React from 'react'
-import logo from '../logo2.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//icon fa-bars for responsive nav
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor: '#012b4d'}}>
-      <div className="container">
-        {/* <a className="navbar-brand" href="#"><img className={logo} src={logo} alt='logo' /></a> */}
-        <a className="navbar-brand logo" href="#"><h1>Iuliia Shevtsova</h1></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <FontAwesomeIcon icon={faBars} style={{color:"white"}}/>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Experience</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contacts</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <>
+    <nav className="navbar-nav">
+      <nav className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <a className="navbar-brand logo" href="#"><h1>Iuliia Shevtsova</h1></a>
+        </Link>
+      </nav>
     </nav>
+    </>
   )
 }
 
