5b35ecbf02e4df84ca1a744a92a52008b13af614
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-04-27 18:42:11 -0400

    color name, chage partials

--- src/App.js
+++ src/App.js
@@ -13,12 +13,16 @@ function App() {
         params={{
           particles: {
             number: {
-              value: 30,
+              value: 20,
               density: {
                 enable: true,
-                value_area: 900
-              }
+                value_area: 800
+              },
+              color: {
+                value: "red"
+              },
             },
+           
             shape: {
               type: "circle",
               stroke: {
@@ -30,6 +34,61 @@ function App() {
           }
         }}
       />
+      {/* <Particles className="particals-canvas"
+        params={{
+          "particles": {
+              "number": {
+                  "value": 100,
+                  "density": {
+                      "enable": false
+                  }
+              },
+              "color": {
+                "value": "rgb(180, 127, 187)"
+              },
+              "size": {
+                  "value": 5,
+                  "random": true,
+                  "anim": {
+                      "speed": 4,
+                      "size_min": 0.5
+                  }
+              },
+              "line_linked": {
+                  "enable": false
+              },
+              "move": {
+                  "random": true,
+                  "speed": 1,
+                  "direction": "top",
+                  "out_mode": "out"
+              }
+          },
+          "interactivity": {
+              "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "repulse"
+                }
+              },
+              "modes": {
+                "bubble": {
+                    "distance": 250,
+                    "duration": 2,
+                    "size": 0,
+                    "opacity": 0,
+                },
+                "repulse": {
+                    "distance": 400,
+                    "duration": 4
+                }
+              }
+          }
+      }} /> */}
       <Navbar />
       <Header />
     </>
