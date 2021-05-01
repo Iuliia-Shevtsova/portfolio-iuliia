40101a48d90b952dc43aea7ec71ee306b3596663
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-05-01 15:20:54 -0400

    add components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,22 +17,22 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="/#about">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
+              <a className="nav-link" href="/#skills">Skills</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Experience</a>
+              <a className="nav-link" href="/#experience">Experience</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Portfolio</a>
+              <a className="nav-link" href="/#portfolio">Portfolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contacts</a>
+              <a className="nav-link" href="/#contacts">Contacts</a>
             </li>
           </ul>
         </div>
