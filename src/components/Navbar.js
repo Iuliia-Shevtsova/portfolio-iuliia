c5715415edd5c36856d5a7c472ee02dff2fa3867
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-05-24 23:52:23 -0400

    delete style nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor: '#012b4d'}}>
+    <nav className="navbar navbar-expand-lg navbar-light " >
       <div className="container">
         {/* <a className="navbar-brand" href="#"><img className={logo} src={logo} alt='logo' /></a> */}
         <a className="navbar-brand logo" href="#"><h1>Iuliia Shevtsova</h1></a>
