d4475cfb827d55bf593b4040159c98681b2ad91e
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-05-25 01:15:28 -0400

    update contact links

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -8,9 +8,9 @@ const Contacts = () => {
       Contacts
       </div> */}
       <div className="contacts">
-      <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-      <i class="fa fa-github" aria-hidden="true"></i>
-      <i class="fa fa-envelope" aria-hidden="true"></i>
+        <a href='https://www.linkedin.com/in/iuliia-shevtsova-b19828198' target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        <a href='https://github.com/Iuliia-Shevtsova' target="_blank" rel="noopener noreferrer"><i class="fa fa-github" aria-hidden="true"></i></a>
+        <a href='mailto:uliyaal2018@gmail.com' target="_blank" rel="noopener noreferrer"><i class="fa fa-envelope" aria-hidden="true"></i></a>
       </div>
     </div>
   )
