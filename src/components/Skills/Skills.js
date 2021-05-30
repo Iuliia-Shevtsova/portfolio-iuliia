518b469bffbfb1c0be6ac0369176d9413ae3d3fb
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-05-29 22:44:13 -0400

    update skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,24 +3,21 @@ import './Skills.css'
 import PageHeader from './../PageHeader/PageHeader'
 
 const Skills = ({projectData}) => {
-  // console.log(projectData[0].fields)
   return (
     <div className='skills-page' id='skills'>
       <PageHeader title={'Skills'}/>
       <div className="row">
         {
-          projectData.map((icon)=>{
-              return (
-                <div className="skills" key={icon.fields.id}>
-                  <div className="icon-box">
-                    <div className="skill-icon">
-                      <img src={icon.fields.iconURL[0].url} alt={icon.fields.icon}></img>
-                    </div>
-                  </div>
-                  <h1>{icon.fields.icon}</h1>
+          projectData.map((icon, index)=>(
+            <div className="skills" key={index}>
+              <div className="icon-box">
+                <div className="skill-icon">
+                  <img src={icon.fields.iconURL[0].url} alt={icon.fields.icon}></img>
                 </div>
-                )
-            })
+              </div>
+              <h1>{icon.fields.icon}</h1>
+            </div>
+          ))
         }
       </div>
     </div>
